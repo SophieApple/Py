08a6e6fdf9dc8006d9040d92f3bff52dae1ffa80
zxingwork <zxingwork@163.com>
2019-08-13 14:23:23 +0800

    update

--- FMStest/test/Fmsapi.py
+++ FMStest/test/Fmsapi.py
@@ -1,8 +1,29 @@
-from .UI import Ui_MianWIndow
+import sys,requests
+# sys.path.append('D:\PycharmProjects\Py\FMStest\test')
+from UI import Ui_MianWIndow
 from PyQt5.QtWidgets import *
 from PyQt5 import QtWidgets,QtCore,QtGui
-import sys,requests
 
 class Show(QMainWindow,Ui_MianWIndow):
     def __init__(self):
         super(Show, self).__init__()
+        self.setupUi(self)
+        self.pushButton_in.clicked.connect(self.ImportExcel)
+
+
+    def ImportExcel(self):
+
+        self.filename,self.filetype = QFileDialog.getOpenFileName(self,"选择文件","./","所有文件 (*);;Excel (.xlsx)")
+        print(self.filename)
+        print(self.filetype)
+
+
+def main():
+    app = QApplication(sys.argv)
+    gui = Show()
+    gui.show()
+    sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    main()
