8c9bce7d2160794eac5828cc313016155b87b205
zxingwork <zxingwork@163.com>
2019-08-13 16:59:09 +0800

    update

--- FMStest/test/Fmsapi.py
+++ FMStest/test/Fmsapi.py
@@ -1,6 +1,6 @@
-import sys,requests,sip
+import sys,requests,sip,json
 # sys.path.append('D:\PycharmProjects\Py\FMStest\test')
-from .UI import Ui_MianWIndow
+from UI import Ui_MianWIndow
 from PyQt5.QtWidgets import *
 from PyQt5 import QtWidgets,QtCore,QtGui
 import pandas as pd
@@ -24,6 +24,7 @@ class Show(QMainWindow,Ui_MianWIndow):
         self.checkBox_query.stateChanged.connect(self.Query)
         self.checkBox_header.stateChanged.connect(self.Header)
 
+
     def ImportExcel(self):
 
         self.filename,self.filetype = QFileDialog.getOpenFileName(self,"选择文件","./","所有文件 (*);;Excel (.xlsx)")
@@ -35,13 +36,45 @@ class Show(QMainWindow,Ui_MianWIndow):
             print('未选择文件')
 
     def ChoicePath(self):
+        self.name = self.comboBox_way.currentText()
         self.path = self.list[self.comboBox_way.currentIndex()][2]
         self.method = self.list[self.comboBox_way.currentIndex()][1]
         self.label_method.setText(self.method)
         print(self.method,self.path)
 
     def Request(self):
-        self.url = 'http://192.168.83.200:8088{}',format(self.path)
+        print('in')
+        self.request_body = ''
+        self.request_header = ''
+        self.request_query = ''
+        Mark = True
+        self.url = 'http://192.168.83.200:8088{}'.format(self.path)
+        if self.checkBox_body.isChecked() and len(self.body_edit.toPlainText()) != 0:
+            try:
+                self.request_body = json.loads(self.body_edit.toPlainText())
+                print(self.request_body)
+            except Exception as e:
+                print(e)
+                Mark = False
+        if self.checkBox_header.isChecked() and len(self.header_edit.toPlainText()) != 0:
+            try:
+                self.request_header = json.loads(self.header_edit.toPlainText())
+            except Exception as e:
+                print(e)
+                Mark = False
+        if self.checkBox_query.isChecked() and len(self.query_edit.toPlainText()) != 0:
+            try:
+                self.request_query = json.loads(self.query_edit.toPlainText())
+            except Exception as e:
+                print(e)
+                Mark = False
+        print('here')
+        print(self.method,self.url)
+        if Mark:
+            response = requests.request(method=self.method,url=self.url,params=self.request_query,headers=self.request_header,data=self.request_body)
+            print(response.content)
+            # self.Result('{}:\t{}'.format(self.name,response.status_code))
+            self.Result(response.text)
 
 
     def Body(self):
@@ -71,8 +104,10 @@ class Show(QMainWindow,Ui_MianWIndow):
             self.header_edit = QtWidgets.QTextEdit()
             self.header_edit.setPlaceholderText('输入Header')
 
-    def Result(self):
-        self.
+    def Result(self,list):
+        self.textBrowser_result.append(list)
+        # self.textBrowser_result.setText(list)
+
 
 def main():
     app = QApplication(sys.argv)
